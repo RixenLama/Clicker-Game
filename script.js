d2d3f9143f85d43187c7ef2a72f6e28c51c277e2
Rixen <Rixenlama@gmail.com>
2023-02-20 16:35:20 +0200

    Small saving thing

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 
-let score = 0
+var score = 0
 
 function addScore(amount){
     score = score + amount;
     document.getElementById("score").innerHTML = score;
+}
+
+function saveGame(){
+    var gameSave = {
+        score : score
+    };
+    localStorage.setItem("gameSave", JSON.stringify(gameSave));
+}
+
+setInterval (function (){
+    saveGame();
+}, 20000)
+
+function loadGame(){
+    var savedGame = JSON.parse(localStorage.getItem("gameSave"));
+    if (typeof savedGame.score !== "undefined") score = savedGame.score;
+}
+
+window.onload = function(){
+    loadGame();
 }
