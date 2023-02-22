ae3ec28b3c5f44c9f9ea493bf0a525bcf1b9f674
Rixen <Rixenlama@gmail.com>
2023-02-22 14:10:59 +0200

    Cat-shop New auto collect thing

--- script.js
+++ script.js
@@ -27,3 +27,18 @@ function loadGame(){
     if (typeof savedGame.score !== "undefined") score = savedGame.score;
     document.getElementById("score").innerHTML = score;
 }
+
+function buyCat(){
+    if (score >= 15) {
+        score = score - 15;
+        document.getElementById("score").innerHTML = score;
+        setInterval (catAbility, 1000)
+    } else {
+        window.alert("Not enough points");s
+    }
+}
+
+function catAbility(){
+    score = score + 2;
+    document.getElementById("score").innerHTML = score;
+}
